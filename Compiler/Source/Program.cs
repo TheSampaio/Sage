032d30a8b73435d588177326a87602902fb19abe
Kellvyn Sampaio <64695440+TheSampaio@users.noreply.github.com>
2024-04-02 23:58:21 -0300

    Lexer Re-Writed
    • Re-writing the compiler in C#;
    • File ".gitignore" updated.

---
+++ Compiler/Source/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Sage
+{
+    class Program
+    {
+        static void Main()
+        {
+            Lexer lexer = new Lexer();
+            lexer.Read("../../Code/Main.sg");
+        }
+    }
+}
